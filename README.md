3b263589d7c8d9daa354b4fc28c1a2d7dea1a9b8
zhi.xiong <zhi.xiong@xtalpi.com>
2018-12-19 11:42:24 +0800

    support horizontal direction

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # **scroll-ease-efficient**
- A function that smoothly scroll the scrollbar to the specified position
+ A function that smoothly scroll the scrollbar to the specified position.
+
+ Support two directions：vertical & horizontal.
+
+ Support three animations: gradually & liner & instant
+
 
 # demo
 https://theoxiong.github.io/scroll-ease-efficient/
@@ -38,19 +43,23 @@ scrollTo(scrollEle, 500, { timingFunction: 'liner', duration: 500})
 
 // specify the factor, it is just for 'gradually' mode, and duration is unuserful
 scrollTo(scrollEle, 500, { timingFunction: 'gradually', factor: 6})
+
+// horizontal direction
+scrollTo(scrollEle, 500, { direction: 'horizontal' })
 ```
 
 # description
-**function scrollTo (ele, pos, [options])**
-* ele &lt;Dom&gt; target scrollable element 
-* pos &lt;Number&gt; specified the position which scroll to
-* options &lt;Object&gt; 
-   * timingFunction &lt;String&gt; specify velocity curve of scrolling, default value is 'linear'
-   * duration &lt;Number&gt; specify time of scrolling, default value is 1000
-   * factor &lt;Number&gt; specify the factor of gradually scrolling, it is only for gradually mode, should less then 100, and more then 1
+**<font size=4>function scrollTo (ele, pos, [options])</font>**
+* ele &lt;<font color=#9acd32>Dom</font>&gt; target scrollable element 
+* pos &lt;<font color=#9acd32>Number</font>&gt; specified the position which scroll to
+* options &lt;<font color=#9acd32>Object</font>&gt; 
+   * direction &lt;<font color=#9acd32>String</font>&gt; specify direction of scrolling, default value is 'vertical'
+   * timingFunction &lt;<font color=#9acd32>String</font>&gt; specify velocity curve of scrolling, default value is 'linear'
+   * duration &lt;<font color=#9acd32>Number</font>&gt; specify time of scrolling, default value is 1000
+   * factor &lt;<font color=#9acd32>Number</font>&gt; specify the factor of gradually scrolling, it is only for gradually mode, should less then 100, and more then 1
 
 
 # obey
 MIT
 # Keywords
-scroll  front-end
+scroll dom js animation front-end web
--- index.js
+++ index.js
@@ -26,113 +26,130 @@ window.cancelAnimationFrame = window.cancelAnimationFrame ||
  * Scroll the element to the specified position, it is apply to vertical direction
  * @param {Dom} ele target element
  * @param {Number} pos specified position
- * @param {Object} options include property: timingFunction duration factor
+ * @param {Object} options include property: direction timingFunction duration factor 
+ *        direction: vertical or horizontal
  *        timingFunction: specify velocity curve of scrolling, default value is 'linear'
  *        duration: specify time of scrolling, default value is 1000,
  *        factor: specify the factor of gradually scrolling, it is only for gradually mode, should less then 100, and more then 1
  */
 function scrollTo (ele, pos, options) {
-    if (!(ele && typeof ele.scrollTop === 'number' && typeof ele.scrollHeight === 'number' && typeof pos === 'number' && pos >= 0)) {
+    if (!(ele && 
+        typeof ele.scrollTop === 'number' &&
+        typeof ele.scrollHeight === 'number' &&
+        typeof ele.scrollLeft === 'number' &&
+        typeof ele.scrollWidth === 'number' &&
+        typeof pos === 'number' && 
+        pos >= 0)) {
         return
     }
-    if (!isScrollable(ele) || pos === ele.scrollTop) {
-        return
-    }
-    let maxScrollDist = ele.scrollHeight - ele.clientHeight
-    if (pos > maxScrollDist) {
-        pos = maxScrollDist
+    let eleInfo = {
+        scrollTop: ele.scrollTop,
+        scrollLeft: ele.scrollLeft,
+        scrollHeight: ele.scrollHeight,
+        scrollWidth: ele.scrollWidth,
+        clientHeight: ele.clientHeight,
+        clientWidth: ele.clientWidth
     }
 
-    if (options && typeof options !== 'object') {
-        return console.error('[scroll.js] options type must be object')
-    }
-    let opt = options || {}
+    let opt = (options && typeof options === 'object') ? options : {}
     opt.timingFunction = opt.timingFunction || 'linear'
-    opt.duration = parseInt(opt.duration) || 1000
-    if (opt.duration < 100) {
-        opt.duration = 100
+    opt.duration = opt.duration > 100 ? parseInt(opt.duration) : 1000
+    opt.direction = opt.direction || 'vertical'
+
+    if (!isScrollable(eleInfo, opt.direction, pos)) {
+        return
     }
+    let maxScrollDist = getScrollDist(eleInfo, opt.direction)
+    pos > maxScrollDist ? pos = maxScrollDist : ''
 
     if (opt.timingFunction === 'gradually') {
-        scrollGradually(ele, pos, opt)
+        scrollGradually(ele, eleInfo, pos, opt)
     } else if (opt.timingFunction === 'linear') {
-        scrollLinear(ele, pos, opt.duration)
+        scrollLinear(ele, eleInfo, pos, opt)
     } else if (opt.timingFunction === 'instant') {
-        scrollInstant(ele, pos)
+        scrollInstant(ele, pos, opt)
     }
 }
 
-function scrollGradually (ele, pos, opt) {
+function scrollGradually (ele, eleInfo, pos, opt) {
     let winFrameId = null
-    let scrollTop = ele.scrollTop || 0
+    let scrollCurr = opt.direction === 'vertical' ? eleInfo.scrollTop : eleInfo.scrollLeft
     let factor = 5
     if (typeof opt.factor === 'number' && opt.factor > 1 && opt.factor < 100) {
         factor = opt.factor
     } else {
-        factor = calcFactor(Math.abs(pos - scrollTop), opt.duration)
+        factor = calcFactor(Math.abs(pos - scrollCurr), opt.duration)
     }
 
     const step = function () {
-        let distance = pos - scrollTop
-        scrollTop = scrollTop + distance / factor
-
+        let distance = pos - scrollCurr
+        scrollCurr += distance / factor
         if (Math.abs(distance) < 2) {
-            ele.scrollTop = pos
+            scrollInstant(ele, pos, opt)
             if (winFrameId) {
                 window.cancelAnimationFrame(winFrameId)
                 winFrameId = null
             }
         } else {
-            ele.scrollTop = scrollTop
+            scrollInstant(ele, scrollCurr, opt)
             winFrameId = window.requestAnimationFrame(step)
         }
     }
     step()
 }
 
-function scrollLinear (ele, pos, duration) {
+function scrollLinear (ele, eleInfo, pos, opt) {
     let winFrameId = null
-    let scrollTop = ele.scrollTop || 0
-    let stepTimes = duration / (1000 / 60)
-    let stepDist = (pos - scrollTop) / stepTimes
+    let scrollCurr = opt.direction === 'vertical' ? eleInfo.scrollTop : eleInfo.scrollLeft
+    let stepTimes = opt.duration / (1000 / 60)
+    let stepDist = (pos - scrollCurr) / stepTimes
     let stepCnt = 0
 
     const step = function () {
-        scrollTop += stepDist
+        scrollCurr += stepDist
         stepCnt++
-        if (stepCnt >= stepTimes || (Math.abs(pos - scrollTop) <= (stepDist + 2))) {
-            ele.scrollTop = pos
+        if (stepCnt >= stepTimes || (Math.abs(pos - scrollCurr) <= (stepDist + 2))) {
+            scrollInstant(ele, pos, opt)
             if (winFrameId) {
                 window.cancelAnimationFrame(winFrameId)
                 winFrameId = null
             }
         } else {
-            ele.scrollTop = scrollTop
+            scrollInstant(ele, scrollCurr, opt)
             winFrameId = window.requestAnimationFrame(step)
         }
     }
     step()
 }
 
-function scrollInstant (ele, pos) {
-    ele.scrollTop = pos
+function scrollInstant (ele, pos, opt) {
+    if (opt.direction === 'vertical') {
+        ele.scrollTop = pos
+    } else {
+        ele.scrollLeft = pos
+    }
 }
 
 /**
  * Check whether the element is scrollable
- * @param {Dom} ele target element
+ * @param {Dom} eleInfo target element information
  * @param {String} direction scroll direction, the default value is 'vertical'
  */
-function isScrollable (ele, direction) {
-    if (!ele) {
-        return false
-    }
-
+function isScrollable (eleInfo, direction, pos) {
     direction = direction || 'vertical'
     if (direction === 'vertical') {
-        return ele.scrollHeight > ele.clientHeight
+        return (eleInfo.scrollHeight > eleInfo.clientHeight) && (pos !== eleInfo.scrollTop)
     } else if (direction === 'horizontal') {
-        return ele.scrollWidth > ele.clientWidth
+        return (eleInfo.scrollWidth > eleInfo.clientWidth) && (pos !== eleInfo.scrollLeft)
+    }
+}
+
+function getScrollDist (eleInfo, direction) {
+    direction = direction || 'vertical'
+    if (direction === 'vertical') {
+        return eleInfo.scrollHeight - eleInfo.clientHeight
+    } else if (direction === 'horizontal') {
+        return eleInfo.scrollWidth - eleInfo.clientWidth
     }
 }
 
